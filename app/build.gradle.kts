15a4d45ab3bb05f74db64604a5cf7c1ee180b830
1fexd <PGFvy85nC@protonmail.com>
2024-07-28 21:30:16 +0200

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,99 @@
+import com.android.build.api.dsl.VariantDimension
+import de.fayard.refreshVersions.core.versionFor
+import fe.buildsrc.*
+import net.nemerosa.versioning.ReleaseInfo
+import net.nemerosa.versioning.SCMInfo
+import net.nemerosa.versioning.VersioningExtension
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "fe.firefoxsync.share"
+    compileSdk = Version.COMPILE_SDK
+
+    defaultConfig {
+        applicationId = "fe.firefoxsync.share"
+        minSdk = Version.MIN_SDK
+        targetSdk = Version.COMPILE_SDK
+
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlin {
+        jvmToolchain(Version.JVM)
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = versionFor(AndroidX.compose.compiler)
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring(Android.tools.desugarJdkLibs)
+
+    implementation(MozillaComponents.concept.storage)
+    implementation(MozillaComponents.concept.toolbar)
+    implementation(MozillaComponents.browser.storageSync)
+    implementation(MozillaComponents.service.firefoxAccounts)
+    implementation(MozillaComponents.service.syncLogins)
+    implementation(MozillaComponents.service.syncAutofill)
+    implementation(MozillaComponents.support.rustLog)
+    implementation(MozillaComponents.support.rustHttp)
+    implementation(MozillaComponents.lib.fetchHttpUrlConnection)
+    implementation(MozillaComponents.lib.dataProtect)
+
+    implementation(AndroidX.core.ktx)
+    implementation(AndroidX.lifecycle.runtime.ktx)
+    implementation(AndroidX.activity.compose)
+
+    implementation(platform(AndroidX.compose.bom))
+    implementation(AndroidX.compose.ui)
+    implementation(AndroidX.compose.ui.graphics)
+    implementation(AndroidX.compose.ui.toolingPreview)
+    implementation(AndroidX.compose.material3)
+
+    testImplementation(Testing.junit4)
+
+    androidTestImplementation(platform(AndroidX.compose.bom))
+    androidTestImplementation(AndroidX.test.ext.junit)
+    androidTestImplementation(AndroidX.test.espresso.core)
+    androidTestImplementation(AndroidX.compose.ui.testJunit4)
+
+    debugImplementation(AndroidX.compose.ui.tooling)
+    debugImplementation(AndroidX.compose.ui.testManifest)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+@file:Suppress("UnstableApiUsage")
+
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+    }
+
+    plugins {
+        id("de.fayard.refreshVersions") version "0.60.5"
+    }
+}
+
+plugins {
+    id("de.fayard.refreshVersions")
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.mozilla.org/maven2") }
+        mavenLocal()
+    }
+}
+
+rootProject.name = "FirefoxSyncShare"
+include(":app")
